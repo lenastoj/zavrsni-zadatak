fb2fcd272c99401934ea4907fcff48548d954bf0
lenastoj <jelenastojkecic@gmail.com>
2022-12-14 21:56:12 +0100

    zadatak 4

---
+++ database.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "127.0.0.1";
+$username = "vivify";
+$password = "vivify";
+$dbname = "blog";
+
+try {
+   
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ }
+ catch(PDOException $e)
+ {
+     echo $e->getMessage();
+ }
+ 
+ function fetchData($connection, $sql, $fetchAll = false) {
+     $statement = $connection->prepare($sql);
+     $statement->execute();
+     $statement->setFetchMode(PDO::FETCH_ASSOC);
+     if($fetchAll) {
+         return $statement->fetchAll();
+     } 
+     
+     return $statement->fetch();
+ }
+ 
+
+ function dump($data)
+ {
+     echo '<pre>';
+     var_dump($data);
+     echo '</pre>';
+     
+ }
+ 
+?>
