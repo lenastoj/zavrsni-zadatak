f4421bee8d6e3bd9666ca2cfacd2b0ebb05db6b9
lenastoj <jelenastojkecic@gmail.com>
2022-12-16 21:09:22 +0100

    zadatak 5 korekcije

--- comments.php
+++ comments.php
@@ -1,8 +1,30 @@
-<?php
-include ('header.php');
-?>
+<form action="" method="POST" class="comment-form">
+    <p class="error">
+        <?php if (isset($error)) {
+            echo $error;
+        } ?>
+    </p>
+    <label for="name">Full name:</label>
+    <input type="text" id="name" name="name">
+    <br>
+    <label for="comment">Your comment:</label>
+    <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+    <!-- <input type="textarea" id="comment"  name="comment"> -->
+    <br>
+    <input type="submit" id="submit" name="submit">
 
-<?php
-include ('sidebar.php');
-include ('footer.php');
-?>
+</form>
+
+
+<div class="comments">
+    <h5>Comments:</h5>
+    <ul>
+        <?php for ($i = 0; $i < count($comments_for_one_post); $i++) { ?>
+            <li>
+                <p>by: <?php echo $comments_for_one_post[$i]['author'] ?></p>
+                <p><?php echo $comments_for_one_post[$i]['text'] ?></p>
+                <hr>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
--- single-post.php
+++ single-post.php
@@ -43,36 +43,10 @@ if(isset($_POST['submit'])){
                 <p><?php echo $one_post_from_db['body']?></p>
             </div>
 
-            <form action="" method="POST" class="comment-form">
-                <p class="error">
-                    <?php if(isset($error)) {
-                        echo $error;
-                        }?>
-                </p>
-                <label for="name">Full name:</label>
-                <input type="text" id="name" name="name">
-                <br>
-                <label for="comment">Your comment:</label>
-                <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
-                <!-- <input type="textarea" id="comment"  name="comment"> -->
-                <br>
-                <input type="submit" id="submit"  name="submit">
-                
-            </form>
-
-            <div class="comments">
-                <h5>Comments:</h5>
-                <ul>
-                <?php for($i = 0; $i < count($comments_for_one_post); $i++ ){ ?>
-                    <li>
-                        <p>by: <?php echo $comments_for_one_post[$i]['author']?></p>
-                        <p><?php echo $comments_for_one_post[$i]['text']?></p>
-                        <hr>
-                    </li>
-                    <?php } ?>
-                </ul>
-            </div>
-            
+       
+            <?php
+            include('comments.php');
+            ?>
             
             <p class = "post-back-link"><a href="posts.php" class = "post-back-link">Go back to All Posts!</a></p>
         </div>
