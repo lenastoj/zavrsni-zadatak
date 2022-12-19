c451a7e22670a78f4e7ed5196fd71144b8fede44
lenastoj <jelenastojkecic@gmail.com>
2022-12-19 18:21:38 +0100

    zadatak 7

--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,17 @@
+<?php
+$select_posts_title = "SELECT id, title FROM posts ORDER BY id DESC LIMIT 5";
+$posts_title = fetchData($connection, $select_posts_title, true);
+// dump($posts_title);
+?>
+
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
             <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                <h4>Latest posts</h4>
+                <?php for($i = 0; $i < count($posts_title); $i++ ){ ?>
+                    <p><a href="single-post.php?post_id=<?php echo($posts_title[$i]['id']) ?>"><?php echo $posts_title[$i]['title'] ?></a></p>
+                <?php } ?>
             </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
-            </div>
+            
         </aside><!-- /.blog-sidebar -->
 
     </div><!-- /.row -->
